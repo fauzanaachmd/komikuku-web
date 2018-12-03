28e7b1b6ad4d9fc6288e63fb4b1eec9917d97a69
Fauzan Achmad <fauzan@nusantarabetastudio.com>
2018-12-04 01:02:40 +0700

    Create serial done, update read count episode, list episode done, episode detail done

--- pages/comic-detail.php
+++ pages/comic-detail.php
@@ -1,10 +1,25 @@
+<?php
+$serial_id = $uri_segment[4];
+$stmt = $conn->prepare("SELECT nama, genre, sinopsis FROM serial WHERE serial_id=$serial_id");
+$stmt->execute();
+
+$serial_res = $stmt->fetch();
+?>
 <div class="container my-5">
     <div class="row mb-3">
         <div class="col-sm-4">
-            <h3>Room Of Swords</h3>
+            <h3><?php echo $serial_res['nama'] ?></h3>
         </div>
+        <?php
+        $episode_id = $uri_segment[6];
+
+        $stmt = $conn->prepare("SELECT name, image, read_count FROM episode WHERE epsiode_id=$episode_id");
+        $stmt->execute();
+
+        $result = $stmt->fetch();
+        ?>
         <div class="col-4 text-sm-center">
-            <h5>#Chapter 0</h5>
+            <h5>#Chapter <?php echo $result['name']; ?></h5>
         </div>
         <div class="col-4 text-right">
             <div class="input-group mb-3">
@@ -22,7 +37,7 @@
     </div>
     <div class="row mb-3">
         <div class="col-12">
-            <img src="http://via.placeholder.com/1140x600" alt="Gambar episode nya" width="100%">
+            <img src="<?php echo getimagesize(ASSET_URL . 'images/eps/' . $result['image']) && $result['image'] != '' ? ASSET_URL . 'images/eps/' . $result['image'] : 'http://via.placeholder.com/100x100'; ?>" alt="<?php echo $result['name']; ?>" width="100%">
         </div>
     </div>
     <div class="row py-4">
@@ -40,3 +55,14 @@
         </div>
     </div>
 </div>
+<?php
+// Update read count
+$read_count = $result['read_count'] + 1;
+$sql = "UPDATE episode SET read_count=$read_count WHERE epsiode_id=$episode_id";
+
+// Prepare statement
+$stmt = $conn->prepare($sql);
+
+// execute the query
+$stmt->execute();
+?>
